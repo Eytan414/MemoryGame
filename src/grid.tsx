762493339a2b81873f15aef7360cc36e9da7b92a
Eytan Ivri <eytanivri@gmail.com>
2022-08-07 19:38:10 +0300

    update algorithm

--- src/grid.tsx
+++ src/grid.tsx
@@ -7,45 +7,23 @@ interface GridProps {
 }
 
 const generateGameData = (size: number):Card[] =>{
-    const cards = Array<Card>();
-    const randomSet = new Set<number>();
+    let evens: Array<Card> = Array<Card>();
     for(let i=0; i<size/2; i++){
-        let match:number;
-        let index = calcNextIndex(randomSet);
-        randomSet.add(index);
-        while(true){
-            let rand = Math.round(Math.random()*(size));
-            if(randomSet.has(rand))
-                continue;
-            match = rand;
-            randomSet.add(rand);
-            break;
-        }
-            
-        const [card, matchingCard]: Card[] = [{
-            index,
-            match,
-            text: index*10+''
-        },{
-            index: match,
-            match: index,
-            text: index*10+''
-        }];
-        cards.push(card);
-        cards.push(matchingCard);
+        const card: Card = {
+            index: i,
+            match: i+1,
+            text: i+''
+        };
+        evens.push(card);
+        const matchingCard: Card = {
+            index: i+1,
+            match: i-1,
+            text: i+''
+        };
+        evens.push(matchingCard);
     }
-    return cards;
-}
 
-const calcNextIndex = (set:Set<number>):number=>{
-    if(set.size === 0) return 0;
-    let lastIndex:number;
-    for(let i=0; i<set.size; i++){//find lowest unoccupied slot
-        lastIndex = i;
-        if(!set.has(i))
-            return i;   
-    }
-    return lastIndex+1;
+    return evens;
 }
 
 export const Grid = (props: GridProps) => {
